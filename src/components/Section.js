ff10a8a7db25cad0311dd83f3b43e2c8517c2815
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-02-04 01:49:22 +0800

    section header text size

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,7 +5,7 @@ const font = Space_Mono({ subsets: ['latin'], weight: ['400', '700']});
 export default function Section({ header = '', className = '', children }) {
     return (
         <div className={'flex flex-col w-full max-w-3xl mb-56 ' + className}>
-            <p className={'flex font-extrabold text-xl gap-4 items-center text-teal-400 tracking-wider ' + font.className}>
+            <p className={'flex font-extrabold sm:text-2xl text-xl gap-4 items-center text-teal-400 tracking-wider ' + font.className}>
                 {header} <span className='bg-teal-600 grow h-px'></span>
             </p>
             
